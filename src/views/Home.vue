9703e80037b05c48a38d81dd3abb18347344b36a
jgpbDev TSC <j.gab2803@gmail.com>
2021-12-22 14:25:50 -0600

    :twisted_rightwards_arrows: Rename branch 'til lesson 5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,28 @@
     <v-btn text color="pink">Click me one more time</v-btn>
     
     <v-btn depressed small class="pink white--text">
-      <v-icon left>mdi-account-box</v-icon>
+      <v-icon left small>mdi-account-box</v-icon>
       <span>Click me one more time</span>
     </v-btn>
 
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email-box</v-icon>
+      <span>Click me bigger</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>Another big button</span>
+      <v-icon right>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn fab depressed small dark color class="purple">
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn large color="blue-grey" dark>
+      Upload
+      <v-icon right dark>mdi-cloud-upload</v-icon>
+    </v-btn>
   </div>
 </template>
 

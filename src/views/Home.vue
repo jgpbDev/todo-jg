d4c79483562f880ee574041f658e0bd39a740fe0
jgpbDev TSC <j.gab2803@gmail.com>
2021-12-22 13:19:00 -0600

    :construction: Practice with buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,11 @@
     <v-btn depressed color="pink">Click me again</v-btn>
     <v-btn text color="pink">Click me one more time</v-btn>
     
-    <v-btn depressed class="pink white--text">
+    <v-btn depressed small class="pink white--text">
       <v-icon left>mdi-account-box</v-icon>
       <span>Click me one more time</span>
     </v-btn>
+
   </div>
 </template>
 

c03231f19edaaddc66e6608202c948213e245db6
jgpbDev TSC <j.gab2803@gmail.com>
2021-12-27 14:14:31 -0600

    :construction: Lesson 17 worked

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,25 @@
 
     <v-container class="my-5">
 
+      <v-row class="mb-3">
+        <v-btn small text color="grey" @click="sortBy('title')">
+          <v-icon small left>
+            mdi-folder
+          </v-icon>
+          <span class="caption text-capitalize">
+            By project/name
+          </span>
+        </v-btn>
+        <v-btn small text color="grey" @click="sortBy('person')">
+          <v-icon small left>
+            mdi-account
+          </v-icon>
+          <span class="caption text-capitalize">
+            By person
+          </span>
+        </v-btn>
+      </v-row>
+
       <v-card flat v-for="project in projects" :key="project.title">
         <v-row dense no-gutters :class="`pa-6 project ${project.status}`"> <!-- Notice the importance of the dense & no-gutters props -->
           <v-flex xs12 md6>
@@ -33,12 +52,17 @@
 export default {
   data: () => ({
     projects: [
-      { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-      { title: 'Code up the homepage', person: 'Chun Li', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-      { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
-      { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      { title: 'Design a new website', person: 'jgpbDev', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      { title: 'Code up the homepage', person: 'Alansín', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      { title: 'Design video thumbnails', person: 'Kevinsillo', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
+      { title: 'Create a community forum', person: 'El Pepe', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
     ]
-  })
+  }),
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
+    }
+  }
 };
 </script>
 

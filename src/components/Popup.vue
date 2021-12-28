09d43623dfdeeda5f4c1b81d812afd2d145e6ef2
jgpbDev-TSC <jorgeg.perezb@gmail.com>
2021-12-28 14:40:14 -0600

    :construction: Lesson24 worked

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -10,6 +10,13 @@
       <v-card-title class="text-h5 grey lighten-2">
         Add a new project
       </v-card-title>
+      <v-card-text>
+        <v-form class="px-3">
+          <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
+          <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil-outline"></v-textarea>
+          <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
+        </v-form>
+      </v-card-text>
     </v-card>
   </v-dialog>
 </template>
@@ -17,8 +24,15 @@
 <script>
 export default {
   data: () => ({
-    dialog: false
-  })
+    dialog: false,
+    title: "", 
+    content: "",
+  }),
+  methods: {
+    submit() {
+      console.log(this.title, this.content)
+    }
+  }
 };
 </script>
 

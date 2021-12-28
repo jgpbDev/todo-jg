34561c8a9eb55b559245c2e5d7e4c2ca07333f1d
jgpbDev-TSC <jorgeg.perezb@gmail.com>
2021-12-28 15:02:42 -0600

    :construction: Lefts date picker listening to computed property

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -7,13 +7,21 @@
     </template>
 
     <v-card>
-      <v-card-title class="text-h5 grey lighten-2">
+      <v-card-title class="text-h5">
         Add a new project
       </v-card-title>
       <v-card-text>
         <v-form class="px-3">
           <v-text-field label="Title" v-model="title" prepend-icon="mdi-folder"></v-text-field>
           <v-textarea label="Information" v-model="content" prepend-icon="mdi-pencil-outline"></v-textarea>
+          
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field text label="Due date" prepend-icon="mdi-calendar-range" :value="due" v-bind="attrs" v-on="on"></v-text-field>
+            </template>
+            <v-date-picker v-model="formattedDate"></v-date-picker>
+          </v-menu>
+          
           <v-btn text class="success mx-0 mt-3" @click="submit">Add project</v-btn>
         </v-form>
       </v-card-text>
@@ -22,15 +30,23 @@
 </template>
 
 <script>
+import format from 'date-fns/format'
+
 export default {
   data: () => ({
     dialog: false,
     title: "", 
     content: "",
+    due: null   //To format the date we installed date-fns with npm
   }),
   methods: {
     submit() {
       console.log(this.title, this.content)
+    }
+  },
+  computed: {
+    formattedDate() {
+      return this.due ? format(this.due, 'do MMM YYYY') : ''
     }
   }
 };

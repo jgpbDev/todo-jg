52e1df48f9e3f706467f2a2c8e388faf191411de
jgpbDev-TSC <jorgeg.perezb@gmail.com>
2021-12-28 14:29:21 -0600

    :construction: Lesson23

---
+++ src/components/Popup.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog v-model="dialog" max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="secondary" elevation="0" v-bind="attrs" v-on="on">
+        Add new project
+      </v-btn>
+    </template>
+
+    <v-card>
+      <v-card-title class="text-h5 grey lighten-2">
+        Add a new project
+      </v-card-title>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false
+  })
+};
+</script>
+
+<style></style>
